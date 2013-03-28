5fd60b6f674910f2499f88de51b2ce4d8adf90d0
Russell Toris <rctoris@wpi.edu>
2013-03-28 12:28:13 -0700

    grid added

--- build/ros3d.js
+++ build/ros3d.js
@@ -6760,6 +6760,35 @@ ROS3D.Axes = function(options) {
 };
 ROS3D.Axes.prototype.__proto__ = THREE.Object3D.prototype;
 /**
+ * @author Russell Toris - rctoris@wpi.edu
+ */
+
+/**
+ * Create a grid object.
+ * 
+ * @constructor
+ * @param options - object with following keys:
+ *  * size (optional) - the size of the grid
+ *  * color (optional) - the line color of the grid, like '#cccccc'
+ *  * lineWidth (optional) - the width of the lines in the grid
+ */
+ROS3D.Grid = function(options) {
+  var options = options || {};
+  var size = options.size || 50;
+  var color = options.color || '#cccccc';
+  var lineWidth = options.lineWidth || 1;
+
+  // create the mesh
+  THREE.Mesh.call(this, new THREE.PlaneGeometry(size, size, size, size),
+      new THREE.MeshBasicMaterial({
+        color : color,
+        wireframe : true,
+        wireframeLinewidth : lineWidth,
+        transparent : true
+      }));
+};
+ROS3D.Grid.prototype.__proto__ = THREE.Mesh.prototype;
+/**
  * @author Jihoon Lee - jihoonlee.in@gmail.com
  * @author Russell Toris - rctoris@wpi.edu
  */
