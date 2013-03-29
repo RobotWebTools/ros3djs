69e59428128c2bde0fdea4e4327ef04a2c4372ea
Russell Toris <rctoris@wpi.edu>
2013-03-28 21:19:46 -0700

    docs and build upaded with maps

--- build/ros3d.js
+++ build/ros3d.js
@@ -1170,6 +1170,115 @@ ROS3D.InteractiveMarkerMenu.prototype.hide = function(event) {
   document.body.removeChild(this.menuDomElem);
 };
 /**
+ * @author Russell Toris - rctoris@wpi.edu
+ */
+
+/**
+ * An OccupancyGrid can convert a ROS occupancy grid message into a THREE object.
+ *
+ * @constructor
+ * @param options - object with following keys:
+ *   * message - the occupancy grid message
+ */
+ROS3D.OccupancyGrid = function(options) {
+  var options = options || {};
+  var message = options.message;
+
+  // create the geometry
+  var width = message.info.width;
+  var height = message.info.height;
+  var geom = new THREE.PlaneGeometry(width, height);
+
+  // create the color material
+  var dataColor = new Uint8Array(width * height * 3);
+  for ( var row = 0; row < height; row++) {
+    for ( var col = 0; col < width; col++) {
+      // determine the index into the map data
+      var mapI = col + ((height - row - 1) * width);
+      // determine the value
+      var data = message.data[mapI];
+      if (data === 100) {
+        var val = 0;
+      } else if (data === 0) {
+        var val = 255;
+      } else {
+        var val = 127;
+      }
+
+      // determine the index into the image data array
+      var i = (col + (row * width)) * 3;
+      // r
+      dataColor[i] = val;
+      // g
+      dataColor[++i] = val;
+      // b
+      dataColor[++i] = val;
+    }
+  }
+  var texture = new THREE.DataTexture(dataColor, width, height, THREE.RGBFormat);
+  texture.needsUpdate = true;
+  var material = new THREE.MeshBasicMaterial({
+    map : texture
+  });
+  material.side = THREE.DoubleSide;
+
+  // create the mesh
+  THREE.Mesh.call(this, geom, material);
+  this.scale.x = message.info.resolution;
+  this.scale.y = message.info.resolution;
+};
+ROS3D.OccupancyGrid.prototype.__proto__ = THREE.Mesh.prototype;
+/**
+ * @author Russell Toris - rctoris@wpi.edu
+ */
+
+/**
+ * A marker client that listens to a given marker topic.
+ * 
+ * Emits the following events:
+ *  * 'change' - there was an update or change in the marker
+ *  
+ * @constructor
+ * @param options - object with following keys:
+ *   * ros - the ROSLIB.Ros connection handle
+ *   * topic (optional) - the map topic to listen to
+ *   * rootObject (optional) - the root object to add this marker to
+ *   * continuous (optional) - if the map should be continuously loaded (e.g., for SLAM)
+ */
+ROS3D.OccupancyGridClient = function(options) {
+  var that = this;
+  var options = options || {};
+  var ros = options.ros;
+  var topic = options.topic || '/map';
+  this.tfClient = options.tfClient;
+  this.rootObject = options.rootObject || new THREE.Object3D();
+
+  // current grid that is displayed
+  this.currentGrid = null;
+
+  // subscribe to the topic
+  var rosTopic = new ROSLIB.Topic({
+    ros : ros,
+    name : topic,
+    messageType : 'nav_msgs/OccupancyGrid',
+    compression : 'png'
+  });
+  rosTopic.subscribe(function(message) {
+    // check for an old marker
+    if (that.currentGrid) {
+      that.rootObject.remove(that.currentGrid);
+    }
+
+    that.currentGrid = new ROS3D.OccupancyGrid({
+      message : message
+    });
+    that.rootObject.add(that.currentGrid);
+
+    that.emit('change');
+  });
+};
+ROS3D.OccupancyGridClient.prototype.__proto__ = EventEmitter2.prototype;
+/**
  * @author David Gossow - dgossow@willowgarage.com
  * @author Russell Toris - rctoris@wpi.edu
  */
