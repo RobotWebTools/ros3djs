RobotWebTools/ros3djs

commit dd381b0a7fa9a17883b41a351ae29e86284d983a

Support for different marker actions

src/markers/MarkerArrayClient.js
@@ -45,21 +45,44 @@ ROS3D.MarkerArrayClient = function(options) {
   arrayTopic.subscribe(function(arrayMessage) {
 
     arrayMessage.markers.forEach(function(message) {
-      var newMarker = new ROS3D.Marker({
-        message : message,
-        path : that.path,
-        loader : that.loader
-      });
-
-      // remove old marker from Three.Object3D children buffer
-      that.rootObject.remove(that.markers[message.ns + message.id]);
-
-      that.markers[message.ns + message.id] = new ROS3D.SceneNode({
-        frameID : message.header.frame_id,
-        tfClient : that.tfClient,
-        object : newMarker
-      });
-      that.rootObject.add(that.markers[message.ns + message.id]);
+      if(message.action === 0) {
+        var updated = false;
+        if(message.ns + message.id in that.markers) { // "MODIFY"
+          updated = that.markers[message.ns + message.id].children[0].update(message);
+          if(!updated) { // "REMOVE"
+              that.rootObject.remove(that.markers[message.ns + message.id]);
+          }
+        }
+        if(!updated) { // "ADD"
+          var newMarker = new ROS3D.Marker({
+            message : message,
+            path : that.path,
+            loader : that.loader
+          });
+          that.markers[message.ns + message.id] = new ROS3D.SceneNode({
+            frameID : message.header.frame_id,
+            tfClient : that.tfClient,
+            object : newMarker
+          });
+          that.rootObject.add(that.markers[message.ns + message.id]);
+        }
+      }
+      else if(message.action === 1) { // "DEPRECATED"
+        console.warn('Received marker message with deprecated action identifier "1"');
+      }
+      else if(message.action === 2) { // "DELETE"
+        that.rootObject.remove(that.markers[message.ns + message.id]);
+        delete that.markers[message.ns + message.id];
+      }
+      else if(message.action === 3) { // "DELETE ALL"
+        for (var m in that.markers){
+          that.rootObject.remove(m);
+        }
+        that.markers = {};
+      }
+      else {
+        console.warn('Received marker message with unknown action identifier "'+message.action+'"');
+      }
     });
     
     that.emit('change');

src/markers/Marker.js
@@ -26,11 +26,20 @@ ROS3D.Marker = function(options) {
   }
 
   THREE.Object3D.call(this);
+  
+  if(message.scale) {
+    this.msgScale = [message.scale.x, message.scale.y, message.scale.z];
+  }
+  else {
+    this.msgScale = [1,1,1];
+  }
+  this.msgColor = [message.color.r, message.color.g, message.color.b, message.color.a];
+  this.msgMesh = undefined;
 
   // set the pose and get the color
   this.setPose(message.pose);
-  var colorMaterial = ROS3D.makeColorMaterial(message.color.r, message.color.g, message.color.b,
-      message.color.a);
+  var colorMaterial = ROS3D.makeColorMaterial(this.msgColor[0],
+      this.msgColor[1], this.msgColor[2], this.msgColor[3]);
 
   // create the object based on the type
   switch (message.type) {
@@ -250,9 +259,10 @@ ROS3D.Marker = function(options) {
          message.color.b !== 0 || message.color.a !== 0) {
         meshColorMaterial = colorMaterial;
       }
+      this.msgMesh = message.mesh_resource.substr(10);
       var meshResource = new ROS3D.MeshResource({
         path : path,
-        resource : message.mesh_resource.substr(10),
+        resource :  this.msgMesh,
         material : meshColorMaterial,
         loader : loader
       });
@@ -293,4 +303,110 @@ ROS3D.Marker.prototype.setPose = function(pose) {
 
   // update the world
   this.updateMatrixWorld();
+};
+
+/**
+ * Update this marker.
+ *
+ * @param message - the marker message
+ * @return true on success otherwhise false is returned
+ */
+ROS3D.Marker.prototype.update = function(message) {
+  // set the pose and get the color
+  this.setPose(message.pose);
+  
+  // Update color
+  if(message.color.r !== this.msgColor[0] ||
+     message.color.g !== this.msgColor[1] ||
+     message.color.b !== this.msgColor[2] ||
+     message.color.a !== this.msgColor[3])
+  {
+      var colorMaterial = ROS3D.makeColorMaterial(
+          message.color.r, message.color.g,
+          message.color.b, message.color.a);
+  
+      switch (message.type) {
+      case ROS3D.MARKER_LINE_STRIP:
+      case ROS3D.MARKER_LINE_LIST:
+      case ROS3D.MARKER_POINTS:
+          break;
+      case ROS3D.MARKER_ARROW:
+      case ROS3D.MARKER_CUBE:
+      case ROS3D.MARKER_SPHERE:
+      case ROS3D.MARKER_CYLINDER:
+      case ROS3D.MARKER_TRIANGLE_LIST:
+      case ROS3D.MARKER_TEXT_VIEW_FACING:
+          this.traverse (function (child){
+              if (child instanceof THREE.Mesh) {
+                  child.material = colorMaterial;
+              }
+          });
+          break;
+      case ROS3D.MARKER_MESH_RESOURCE:
+          var meshColorMaterial = null;
+          if(message.color.r !== 0 || message.color.g !== 0 ||
+             message.color.b !== 0 || message.color.a !== 0) {
+              meshColorMaterial = this.colorMaterial;
+          }
+          this.traverse (function (child){
+              if (child instanceof THREE.Mesh) {
+                  child.material = meshColorMaterial;
+              }
+          });
+          break;
+      case ROS3D.MARKER_CUBE_LIST:
+      case ROS3D.MARKER_SPHERE_LIST:
+          // TODO Support to update color for MARKER_CUBE_LIST & MARKER_SPHERE_LIST
+          return false;
+      default:
+          return false;
+      }
+      
+      this.msgColor = [message.color.r, message.color.g,
+            message.color.b, message.color.a];
+  }
+  
+  // Update geometry
+  var scaleChanged =
+        Math.abs(this.msgScale[0] - message.scale.x) > 1.0e-6 ||
+        Math.abs(this.msgScale[1] - message.scale.y) > 1.0e-6 ||
+        Math.abs(this.msgScale[2] - message.scale.z) > 1.0e-6;
+  this.msgScale = [message.scale.x, message.scale.y, message.scale.z];
+  
+  switch (message.type) {
+    case ROS3D.MARKER_CUBE:
+    case ROS3D.MARKER_SPHERE:
+    case ROS3D.MARKER_CYLINDER:
+        if(scaleChanged) {
+            return false;
+        }
+        break;
+    case ROS3D.MARKER_TEXT_VIEW_FACING:
+        if(scaleChanged || this.text !== message.text) {
+            return false;
+        }
+        break;
+    case ROS3D.MARKER_MESH_RESOURCE:
+        var meshResource = message.mesh_resource.substr(10);
+        if(meshResource !== this.msgMesh) {
+            return false;
+        }
+        if(scaleChanged) {
+            return false;
+        }
+        break;
+    case ROS3D.MARKER_ARROW:
+    case ROS3D.MARKER_LINE_STRIP:
+    case ROS3D.MARKER_LINE_LIST:
+    case ROS3D.MARKER_CUBE_LIST:
+    case ROS3D.MARKER_SPHERE_LIST:
+    case ROS3D.MARKER_POINTS:
+    case ROS3D.MARKER_TRIANGLE_LIST:
+        // TODO: Check if geometry changed
+        return false;
+    default:
+        break;
+  }
+  
+  return true;
 };

commit b1b3804e7b18d705619a26ccf917bdf1f83400f6

markers: fixed bug in deleting markers frome scene.
Removing all markers from the scene did not work. Must
remove the object from scene, not the 'key'.

The bug was introduced in dd381b0a7 and was kept during
the merge of 372c018 .

Signed-off-by: Peter Soetens <peter@thesourceworks.com>

src/markers/MarkerArrayClient.js
@@ -79,7 +79,7 @@ ROS3D.MarkerArrayClient = function(options) {
       else if(message.action === 3) { // "DELETE ALL"
         for (var m in that.markers){
             if (that.markers.hasOwnProperty(m)) {
-                that.rootObject.remove(m);
+                that.rootObject.remove( that.markers[m] );
                 that.markers[m].removeTF();
             }
         }
@@ -102,7 +102,7 @@ ROS3D.MarkerArrayClient = function(options) {
 	mac.arrayTopic.unsubscribe();
         for (var key in mac.markers) {
             if (mac.markers.hasOwnProperty(key)) {
-                mac.rootObject.remove( key );
+                mac.rootObject.remove( mac.markers[key] );
                 mac.markers[key].removeTF();
             }
         }