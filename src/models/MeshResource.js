012576eac25814307a3a6fce7971cbaf270752d6
David Lu <davidvlu@gmail.com>
2015-09-07 17:19:41 -0400

    Use color if it is defined

--- src/models/MeshResource.js
+++ src/models/MeshResource.js
@@ -59,11 +59,7 @@ ROS3D.MeshResource = function(options) {
       // add a texture to anything that is missing one
       if(material !== null) {
         var setMaterial = function(node, material) {
-          // do not overwrite the material
-          if (typeof node.material === 'undefined') {
-            node.material = material;
-          }
-          //node.material = material;
+          node.material = material;
           if (node.children) {
             for (var i = 0; i < node.children.length; i++) {
               setMaterial(node.children[i], material);
