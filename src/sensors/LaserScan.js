b1f8247017b75b8fe1ca4ba6fe4396f149abf46d
Mathieu Brédif <mathieu.bredif@ign.fr>
2018-04-17 02:15:16 +0200

    fix: pointRatio option in PointCloud2 and LaserScan (#223)

--- src/sensors/LaserScan.js
+++ src/sensors/LaserScan.js
@@ -53,7 +53,7 @@ ROS3D.LaserScan.prototype.processMessage = function(message){
   }
   var n = message.ranges.length;
   var j = 0;
-  for(var i=0;i<n;i+=this.points.positionRatio){
+  for(var i=0;i<n;i+=this.points.pointRatio){
     var range = message.ranges[i];
     if(range >= message.range_min && range <= message.range_max){
         var angle = message.angle_min + i * message.angle_increment;
