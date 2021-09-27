04c5dfb0617ee077fe65f42cb4ad0ec717e238bf
MatthijsBurgh <MatthijsBurgh@users.noreply.github.com>
2021-09-27 20:01:04 +0000

    Update Build

--- build/ros3d.js
+++ build/ros3d.js
@@ -10,14 +10,12 @@ var ROS3D = (function (exports, ROSLIB) {
 					var d = Object.getOwnPropertyDescriptor(e, k);
 					Object.defineProperty(n, k, d.get ? d : {
 						enumerable: true,
-						get: function () {
-							return e[k];
-						}
+						get: function () { return e[k]; }
 					});
 				}
 			});
 		}
-		n['default'] = e;
+		n["default"] = e;
 		return Object.freeze(n);
 	}
 
@@ -59379,4 +59377,4 @@ var ROS3D = (function (exports, ROSLIB) {
 
 	return exports;
 
-}({}, ROSLIB));
+})({}, ROSLIB);
