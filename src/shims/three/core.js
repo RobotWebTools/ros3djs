5ba46866aa46a504c8e3fec28206a7d7da2d5532
Joe Pea <joe@trusktr.io>
2021-10-05 14:03:15 -0700

    Expose THREE as a global, otherwise people can't access Three.js APIs, or they end up installing a second version of Three.js which can lead to issues where singletons are expected, or they'll copy and fork a local ros3d lib, etc.

--- src/shims/three/core.js
+++ src/shims/three/core.js
@@ -1,3 +1,15 @@
-import * as THREE from 'three/build/three.module.js';
+import * as _THREE from 'three/build/three.module.js';
 
-export default Object.assign({}, THREE)
+const THREE = Object.assign({}, _THREE);
+export default THREE;
+
+// While we are at it (bundling Three.js into ROS3D, which is something we need
+// to avoid in a future release so that users can pass their version of THREE
+// from the outside), we should at least expose it as a global so that they
+// can use the THREE global variable, otherwise they have no way to use
+// Three.js APIs that ROS3D embeds inside of it, and therefore the user may do
+// something like import a second version of Three.js which can cause issues
+// (I've seen this problem happen in real-world ROS3D projects, especially with
+// roboticists or data scientists that are inexperienced with web
+// development).
+globalThis.THREE = THREE;
