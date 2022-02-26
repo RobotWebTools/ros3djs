f7a01ab191748c559188206f93c8981b7e62df9e
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-02-26 10:50:02 +0100

    Fix Octree circular dep
    Move Octree Enums to OctreeBase

--- src/navigation/OcTree.js
+++ src/navigation/OcTree.js
@@ -3,34 +3,6 @@
  */
 
 /**
- * Toggles voxel visibility
- *
- *    * `occupied` - only voxels that are above or equal to the occupation threshold are shown
- *    * `free` - only voxels that are below the occupation threshold are shown
- *    * `all` - all allocated voxels are shown
- */
-ROS3D.OcTreeVoxelRenderMode = {
-  OCCUPIED: 'occupied',
-  FREE: 'free',
-  ALL: 'all',
-};
-
-/**
- * Coloring modes for each voxel
- *
- *     * 'solid' - voxels will have a single solid color set by the tree globally
- *     * 'occupancy' - voxels are false colored by their occupancy value. Fall back for `solid` if not available.
- *     * 'color' - voxels will colorized by their
- */
-ROS3D.OcTreeColorMode = {
-  SOLID: 'solid',
-  OCCUPANCY: 'occupancy',
-  COLOR: 'color'
-};
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-/**
  * Specilaization of BaseOcTree
  *
  * @constructor
