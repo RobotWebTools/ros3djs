ddf177371c9aab9c3b2f21de8959e9d5827b3a89
techming <hmeng29@wisc.edu>
2022-11-22 14:16:40 -0600

    Add test to Viewer without canvas

--- test/visualization/Viewer.test.js
+++ test/visualization/Viewer.test.js
@@ -1,6 +1,25 @@
 var assert = chai.assert;
 
 describe('Viewer', function() {
+  const WIDTH = 500;
+  const HEIGHT = 300;
+  const DIV_ID = 'ros3d';
+
+  const div = document.createElement('div');
+  div.id = DIV_ID;
+  const viewer = new ROS3D.Viewer({
+    divID: DIV_ID,
+    height: HEIGHT,
+    width: WIDTH
+  });
+
+  it('initializes correctly with canvas passed in as argument', function() {
+    assert.isTrue(true);
+  });
+
+});
+
+describe('Viewer with canvas as argument', function() {
     const WIDTH = 500;
     const HEIGHT = 300;
     const canvas = document.createElement('canvas');
